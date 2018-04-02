80d45710af614ffe74b66ef0372f1e4ca5a4c56a
IshwarKanse <ikanse@redhat.com>
2018-04-02 18:27:58 +0530

    Added kubectl completion for bash

--- provision/master_init.sh
+++ provision/master_init.sh
@@ -8,3 +8,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+echo 'source <(kubectl completion bash)' >> $HOME/.bashrc
