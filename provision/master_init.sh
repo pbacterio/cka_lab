18af69a98dee1482323851c8d0c940cbd8148d03
IshwarKanse <ikanse@redhat.com>
2018-04-05 10:58:03 +0530

    Fixed Flannel to use eth1 interface when using Vagrant install

--- provision/master_init.sh
+++ provision/master_init.sh
@@ -9,6 +9,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 FLANNEL_VERSION=v0.9.1
 curl -o /tmp/kube-flannel.yml https://raw.githubusercontent.com/coreos/flannel/$FLANNEL_VERSION/Documentation/kube-flannel.yml
-sed -i.bak 's|"/opt/bin/flanneld",|"/opt/bin/flanneld", "--iface=eth1",|' /vagrant/kube-flannel.yml
+sed -i.bak 's|"/opt/bin/flanneld",|"/opt/bin/flanneld", "--iface=eth1",|' /tmp/kube-flannel.yml
 kubectl apply -f /tmp/kube-flannel.yml
 echo 'source <(kubectl completion bash)' >> $HOME/.bashrc
